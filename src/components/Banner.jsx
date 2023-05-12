a48c02ab42283e1d4c1953189a94fa1644fd8ef0
Manuimfeld <manu_jadir@hotmail.com>
2023-05-12 12:53:14 -0300

    Create Banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,17 @@
+const Banner = () => {
+  return (
+    <section className="banner">
+        <div className="banner-glass"></div>
+        <div className="banner-info">
+            <h2>Moana</h2>
+            <p>Moana is a sea-loving, strong-willed wayfarer. Though she has moments of self-doubt, she has great pride in who she is, and doesn’t back away from new challenges. She approaches new experiences and tasks with the utmost seriousness and will stand her ground to fight for what she values, even when all seems lost.</p>
+            <div className="button-container">
+            <button>Ver AHORA</button>
+            <button>Ver Trailer</button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Banner
