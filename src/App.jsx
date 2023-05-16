e9f5eba3e87c3ef14faeae1323e9ca912145b1d5
Manuimfeld <manu_jadir@hotmail.com>
2023-05-16 17:17:53 -0300

    Add call API moviedb and test this

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Banner from './components/Banner'
 import Header from './components/Header'
+import LatestMovies from './components/LatestMovies'
 
 function App() {
 
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    const options = {method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDg5MWQzZjJjN2I5YWNiZGNhNzA5NTAyN2UyOTA3NiIsInN1YiI6IjYyZTc0YTVlNjc4MjU5MDA1OGZhMTk3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W-XsOe2vA8yn2s2Yr5-vCj85kcgvWj2f39Q3fWbFkt0'
+    }}
+
+    fetch('https://api.themoviedb.org/3/trending/movie/day?language=es-ES', options)
+    .then(response => response.json())
+    .then(response => setMovies([response.results]))
+    .catch(err => console.error(err));
+  }, [])
+
   return (
     <>
-      <Header />
+      <Header/>
       <Banner />
+      <LatestMovies movies={movies}/>
     </>
   )
 }
