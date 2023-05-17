7fb2798159b856b9a22d0d46cae1366d9a3810a5
Manuimfeld <manu_jadir@hotmail.com>
2023-05-17 15:22:46 -0300

    Add dinamyc route with click in a movie card

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 const CardMovie = ({movieData}) => {
+
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    const props = movieData
+    navigate(`/movie/${movieData.id}`, {state: props})
+  }
 
   return (
     <article className="card-movie">
             <span>1</span>
-            { <img src={`https://image.tmdb.org/t/p/original${movieData.poster_path}`} alt="" /> }
+            { <img src={`https://image.tmdb.org/t/p/original${movieData.poster_path}`} alt="" onClick={handleClick}/> }
             <div className="movie-info">
             {<p>{movieData.title}</p> }
             <p>2023</p>
