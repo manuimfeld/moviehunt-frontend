0663890499a557fad58296b643f7a07252f9a410
Manuimfeld <manu_jadir@hotmail.com>
2023-05-17 15:23:22 -0300

    Create new view: movie

--- src/views/Movie.jsx
+++ src/views/Movie.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 const Movie = () => {
+
+  const location = useLocation();
+  const props = location.state;
+
+  const [movieData, setMovieData] = useState({})
+
+  useEffect(() => {
+    if (props !== null) {
+      setMovieData(props)
+    }
+
+    console.log("render")
+  }, [props, movieData])
+
+
+
   return (
-    <div>Movie</div>
+    <main className='main-movie'>
+      <h2>{movieData.title}</h2>
+    </main>
   )
 }
 
