dfb90ba760d4081f5d241fef165fafc919b65843
Manuimfeld <manu_jadir@hotmail.com>
2023-05-19 19:11:13 -0300

    Add api endpoint for movie details

---
+++ src/api/getMovieDetails.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { getDetail } from "../redux/detailSlice";
+
+export const getMovieDetailsApi = (id, dispatch) => {
+  axios
+    .get(`https://api.themoviedb.org/3/movie/${id}?language=es-ES`, {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+      },
+    })
+    .then((response) => dispatch(getDetail(response.data)))
+    .catch((error) => console.error(error));
+};
