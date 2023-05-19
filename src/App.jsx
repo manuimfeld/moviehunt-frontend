fabf21c9db2a58a73916f45c04dc32cbaab726e6
Manuimfeld <manu_jadir@hotmail.com>
2023-05-19 19:09:36 -0300

    Refactory App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,35 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Header from './components/Header'
+import { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { getMoviesApi } from './api/getMovies';
+//Components and views
+import Header from './components/Header'
 import Home from './views/Home';
 import Error from './views/Error';
 import Movie from './views/Movie';
+//Styles
+import './App.css'
+
 
 function App() {
-
-  const [movies, setMovies] = useState([])
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const options = {method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDg5MWQzZjJjN2I5YWNiZGNhNzA5NTAyN2UyOTA3NiIsInN1YiI6IjYyZTc0YTVlNjc4MjU5MDA1OGZhMTk3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W-XsOe2vA8yn2s2Yr5-vCj85kcgvWj2f39Q3fWbFkt0'
-    }}
-
-    fetch('https://api.themoviedb.org/3/trending/movie/day?language=es-ES', options)
-    .then(response => response.json())
-    .then(response => setMovies([response.results]))
-    .catch(err => console.error(err));
-  }, [])
+    // Get popular movies for Home view
+    getMoviesApi(dispatch)
+  }, [dispatch])
 
   return (
     <>
       <BrowserRouter>
-      <Header/>
-      <Routes>
-      <Route exact path="/" element={<Home movies={movies}/>} />
-      <Route path="/home" element={<Home movies={movies}/>} />
-{/*       <Route path="/search/:id" element={<Home movies={movies}/>} /> */}
-      <Route path="/movie/:id" element={<Movie/>} />
-      <Route path="*" element={<Error />} />
-      </Routes>
+        <Header/>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/search/:id" element={<Home />} />
+            <Route path="/movie/:id" element={<Movie />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
       </BrowserRouter>
     </>
   )
