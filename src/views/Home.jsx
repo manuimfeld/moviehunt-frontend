cc500f160ead82046f80cff3057c3efaae7a4bdb
Manuimfeld <manu_jadir@hotmail.com>
2023-05-19 19:08:54 -0300

    Refactory Home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,14 @@
 import Banner from '../components/Banner'
 import LatestMovies from '../components/LatestMovies'
-import PropTypes from 'prop-types';
 
-const Home = ({movies}) => {
+
+const Home = () => {
+
   return (
     <>
     <Banner />
-    <LatestMovies movies={movies}/>
+    <LatestMovies/>
     </>
   )
 }
-
-Home.propTypes = {
-    movies: PropTypes.array.isRequired
-  };
-
 export default Home
