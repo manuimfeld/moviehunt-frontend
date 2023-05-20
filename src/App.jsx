06b8314196246d8b41258970eea62f121e2f8147
Manuimfeld <manu_jadir@hotmail.com>
2023-05-20 17:00:53 -0300

    Add new rutes

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,21 @@ import Error from './views/Error';
 import Movie from './views/Movie';
 //Styles
 import './App.css'
+import Favorites from './views/Favorites';
+import initializeLocalStorage from './helpers/initializeStorage';
+import Search from './views/Search';
 
 
 function App() {
   const dispatch = useDispatch();
 
+  // If on app run, localStorage is empty, create array favorites
+  initializeLocalStorage()
+
   useEffect(() => {
     // Get popular movies for Home view
     getMoviesApi(dispatch)
+
   }, [dispatch])
 
   return (
@@ -26,8 +33,10 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/search/:id" element={<Home />} />
+            <Route path="/search/:id" element={<Search />} />
+            <Route path="/search" element={<Search />} />
             <Route path="/movie/:id" element={<Movie />} />
+            <Route path="/favorites" element={<Favorites />} />
             <Route path="*" element={<Error />} />
           </Routes>
       </BrowserRouter>
