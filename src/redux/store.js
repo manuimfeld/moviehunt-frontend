1e4c2a3e2822397cffbacc15b90bf35ba22247d6
Manuimfeld <manu_jadir@hotmail.com>
2023-05-20 16:59:07 -0300

    Add reducers in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import trendingReducer from "./trendingSlice";
 import movieReducer from "./detailSlice";
 import similarReducer from "./similarSlice";
+import favoriteReducer from "./favoriteSlice";
+import searchReducer from "./searchSlice";
 
 export const store = configureStore({
   reducer: {
     moviesArr: trendingReducer,
     movieData: movieReducer,
     similarArr: similarReducer,
+    favoriteArr: favoriteReducer,
+    searchArr: searchReducer,
   },
 });
