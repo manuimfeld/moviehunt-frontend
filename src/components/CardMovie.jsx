89631105f3d218a017ffb9155689b7f013f0d366
Manuimfeld <manu_jadir@hotmail.com>
2023-05-20 16:57:32 -0300

    Refactory component, and add vote points movie

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,8 +1,13 @@
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
+import toggleFavorite from "../helpers/toggleFavorite"
+import { useDispatch } from "react-redux"
+import { FaHeart } from "react-icons/fa"
+
 
 const CardMovie = ({movieData}) => {
 
+  const dispatch = useDispatch();
   const navigate = useNavigate()
 
   const handleClick = () => {
@@ -10,14 +15,18 @@ const CardMovie = ({movieData}) => {
     navigate(`/movie/${movieData.id}`, {state: props})
   }
 
+  const handleFavorite = () => {
+    toggleFavorite(movieData, dispatch)
+  }
+
   return (
     <article className="card-movie">
-            <span>1</span>
+            <span>{movieData.vote_average.toFixed(1)}</span>
             { <img src={`https://image.tmdb.org/t/p/original${movieData.poster_path}`} alt="" onClick={handleClick}/> }
             <div className="movie-info">
             {<p>{movieData.title}</p> }
             <p>2023</p>
-            <p>Like</p>
+            <button onClick={handleFavorite}><FaHeart /></button>
             </div>
     </article>
   )
